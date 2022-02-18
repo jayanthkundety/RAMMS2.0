52dbf7709d6ec8f841aaa424cb386529e289be3b
varadharaj s <varadharaja.p@avowstech.com>
2022-02-18 21:14:15 +0530

    w1

--- RAMS/Web/RAMMS.DTO/ResponseBO/FormW1ResponseDTO.cs
+++ RAMS/Web/RAMMS.DTO/ResponseBO/FormW1ResponseDTO.cs
@@ -7,91 +7,87 @@ namespace RAMMS.DTO.ResponseBO
 {
     public class FormW1ResponseDTO
     {
-
         public int PkRefNo { get; set; }
         public string FormhRefNo { get; set; }
         public string ServPropRefNo { get; set; }
         public string ServPropName { get; set; }
-        public string ServOffice { get; set; }
         public string ServAddress1 { get; set; }
         public string ServAddress2 { get; set; }
         public string ServAddress3 { get; set; }
         public string ServPhone { get; set; }
         public string ServFax { get; set; }
-        public string ReferenceNo { get; set; }
-        public string Division { get; set; }
-        public string Rmu { get; set; }
+        public string IwRefNo { get; set; }
+        public string DivnCode { get; set; }
+        public string RmuCode { get; set; }
         public string ProjectTitle { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? InitialProposedDate { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
-        public DateTime? TecmDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? TecmDt { get; set; }
         public string TecmStatus { get; set; }
         public bool? IsBq { get; set; }
         public bool? IsDrawing { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
-        public DateTime? Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? Dt { get; set; }
         public string RoadCode { get; set; }
         public string RoadName { get; set; }
         public int? ChKm { get; set; }
-      
         public int? ChM { get; set; }
-        public string ChainageApprovedIwref { get; set; }
         public string DetailsOfWork { get; set; }
-        public decimal? PropDesignDuration { get; set; }
-        public decimal? PropCompletionPeriod { get; set; }
-        public string RecommendedInstructedWork { get; set; }
-        public string RecommendedByDe { get; set; }
-        public string RecommendedStatus { get; set; }
-        public decimal? PhyWorks { get; set; }
-        public decimal? GenPrelims { get; set; }
-        public decimal? ConsulTaxPercent { get; set; }
-        public decimal? ConsulFee { get; set; }
-        public decimal? SurvyWorksPercent { get; set; }
-        public decimal? SurvyWorks { get; set; }
-        public decimal? SiteInvestPercent { get; set; }
-        public decimal? SiteInvest { get; set; }
+        public double? PropDesignDuration { get; set; }
+        public double? PropCompletionPeriod { get; set; }
+        public string RecomdYn { get; set; }
+        public string RecomdBydeYn { get; set; }
+        public string RecomdStatus { get; set; }
+        public double? PhyWorksAmt { get; set; }
+        public double? GenPrelimsAmt { get; set; }
+        public double? ConsulTaxPercent { get; set; }
+        public double? ConsulFeeAmt { get; set; }
+        public double? SurvyWorksPercent { get; set; }
+        public double? SurvyWorksAmt { get; set; }
+        public double? SiteInvestPercent { get; set; }
+        public double? SiteInvestAmt { get; set; }
         public string OtherCostLabel { get; set; }
-        public decimal? OtherCost { get; set; }
-        public decimal? EstimTotalCost { get; set; }
-        public int? ReportedBy { get; set; }
-        public string ReportedName { get; set; }
-        public string ReportedDesig { get; set; }
-        public bool ReportedSign { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public double? OtherCostAmt { get; set; }
+        public double? EstimTotalCostAmt { get; set; }
+        public int? UseridRep { get; set; }
+        public string UsernameRep { get; set; }
+        public string DesignationRep { get; set; }
+        public bool SignRep { get; set; }
         [DataType(DataType.Date)]
-        public DateTime? ReportedDate { get; set; }
-        public int? RequestedBy { get; set; }
-        public bool RequestedBySign { get; set; }
-        public string RequestedByName { get; set; }
-        public string RequestedByDesig { get; set; }
-        public string RequestedByOffice { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DtRep { get; set; }
+        public int? UseridReq { get; set; }
+        public bool SignReq { get; set; }
+        public string UsernameReq { get; set; }
+        public string DesignationReq { get; set; }
+        public string OfficeReq { get; set; }
         [DataType(DataType.Date)]
-        public DateTime? RequestedByDate { get; set; }
-        public int? VerifiedBy { get; set; }
-        public bool VerifiedSign { get; set; }
-        public string VerifiedName { get; set; }
-        public string VerifiedDesig { get; set; }
-        public string VerifiedOffice { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DtReq { get; set; }
+        public int? UseridVer { get; set; }
+        public bool SignVer { get; set; }
+        public string UsernameVer { get; set; }
+        public string DesignationVer { get; set; }
+        public string OfficeVer { get; set; }
         [DataType(DataType.Date)]
-        public DateTime? VerifiedDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? DtVer { get; set; }
         public int? ModBy { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? ModDt { get; set; }
         public int? CrBy { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? CrDt { get; set; }
         public bool SubmitSts { get; set; }
         public bool? ActiveYn { get; set; }
         public string Status { get; set; }
         public string AuditLog { get; set; }
-
 
     }
 }
