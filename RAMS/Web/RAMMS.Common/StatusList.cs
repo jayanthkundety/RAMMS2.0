0f411652a08eaff7730bda079ad46917f52ec80b
varadharaj s <varadharaja.p@avowstech.com>
2022-03-05 19:09:48 +0530

    w1

--- RAMS/Web/RAMMS.Common/StatusList.cs
+++ RAMS/Web/RAMMS.Common/StatusList.cs
@@ -40,7 +40,8 @@ namespace RAMMS.Common
 
         public const string FormW1Saved = "Saved";
         public const string FormW1Submitted = "Submitted";
-        public const string FormW1Verified = "Verified";
+        public const string FormW1Rejected = "Rejected";
+        public const string FormW1Approved = "Approved";
 
         public const string FormW2Submitted = "Submitted";
         public const string FormW2Saved = "Saved";
