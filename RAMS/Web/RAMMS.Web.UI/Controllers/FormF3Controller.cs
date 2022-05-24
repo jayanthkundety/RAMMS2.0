fe17fb49300af74159809103a2794508c56ade8a
varadharaj s <varadharaja.p@avowstech.com>
2022-05-24 23:03:18 +0530

    f3

---
+++ RAMS/Web/RAMMS.Web.UI/Controllers/FormF3Controller.cs
@@ -0,0 +1,25 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RAMMS.Business.ServiceProvider.Interfaces;
+using RAMMS.Domain.Models;
+using RAMMS.DTO.JQueryModel;
+using RAMMS.DTO.RequestBO;
+using RAMMS.Web.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+namespace RAMMS.Web.UI.Controllers
+{
+    public class FormF3Controller : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
