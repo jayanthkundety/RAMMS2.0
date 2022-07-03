7820833b43664f489e3e134582e8b491a2cbc7e4
Sakthivel <sakthivel.p@avowstech.com>
2022-07-03 22:18:44 +0530

    File added

---
+++ RAMS/Web/RAMMS.Domain/Models/RmFormF1Dtl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RAMMS.Domain.Models
+{
+    public partial class RmFormF1Dtl
+    {
+        public int Ff1dPkRefNo { get; set; }
+        public int? Ff1dFf1hPkRefNo { get; set; }
+        public int? Ff1dR1hPkRefNo { get; set; }
+        public int? Ff1dLocCh { get; set; }
+        public int? Ff1dLocChDeci { get; set; }
+        public string Ff1dCode { get; set; }
+        public int? Ff1dTier { get; set; }
+        public decimal? Ff1dTotalLength { get; set; }
+        public decimal? Ff1dTopWidth { get; set; }
+        public decimal? Ff1dBottomWidth { get; set; }
+        public decimal? Ff1dHeight { get; set; }
+        public int? Ff1dOverallCondition { get; set; }
+        public string Ff1dDescription { get; set; }
+        public int? Ff1dModBy { get; set; }
+        public DateTime? Ff1dModDt { get; set; }
+        public int? Ff1dCrBy { get; set; }
+        public DateTime? Ff1dCrDt { get; set; }
+        public string Ff1dStatus { get; set; }
+        public string Ff1dAuditLog { get; set; }
+
+        public virtual RmFormF1Hdr Ff1dFf1hPkRefNoNavigation { get; set; }
+    }
+}
---
+++ RAMS/Web/RAMMS.Domain/Models/RmFormF1Hdr.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RAMMS.Domain.Models
+{
+    public partial class RmFormF1Hdr
+    {
+        public RmFormF1Hdr()
+        {
+            RmFormF1Dtl = new HashSet<RmFormF1Dtl>();
+        }
+
+        public int Ff1hPkRefNo { get; set; }
+        public string Ff1hPkRefId { get; set; }
+        public string Ff1hDivCode { get; set; }
+        public string Ff1hAssetId { get; set; }
+        public string Ff1hDist { get; set; }
+        public string Ff1hRmuCode { get; set; }
+        public string Ff1hRdCode { get; set; }
+        public string Ff1hRdName { get; set; }
+        public string Ff1hSecCode { get; set; }
+        public string Ff1hSecName { get; set; }
+        public string Ff1hCrewSup { get; set; }
+        public string Ff1hCrewName { get; set; }
+        public int? Ff1hInspectedYear { get; set; }
+        public DateTime? Ff1hInspectedDate { get; set; }
+        public int? Ff1hInspectedBy { get; set; }
+        public string Ff1hInspectedName { get; set; }
+        public bool? Ff1hInspectedBySign { get; set; }
+        public decimal? Ff1hRoadLength { get; set; }
+        public int? Ff1hConditionI { get; set; }
+        public int? Ff1hConditionIi { get; set; }
+        public int? Ff1hConditionIii { get; set; }
+        public int? Ff1hModBy { get; set; }
+        public DateTime? Ff1hModDt { get; set; }
+        public int? Ff1hCrBy { get; set; }
+        public DateTime? Ff1hCrDt { get; set; }
+        public bool? Ff1hSubmitSts { get; set; }
+        public bool? Ff1hActiveYn { get; set; }
+        public string Ff1hStatus { get; set; }
+        public string Ff1hAuditLog { get; set; }
+
+        public virtual ICollection<RmFormF1Dtl> RmFormF1Dtl { get; set; }
+    }
+}
